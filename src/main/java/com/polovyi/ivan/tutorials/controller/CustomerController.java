616f649e18ad51240649816e16012f3c401f8cdb
ivan.polovyi <polovyivan@yahoo.com>
2023-01-11 10:05:55 -0300

    refactored controller to use class over record

--- src/main/java/com/polovyi/ivan/tutorials/controller/CustomerController.java
+++ src/main/java/com/polovyi/ivan/tutorials/controller/CustomerController.java
@@ -3,22 +3,21 @@ package com.polovyi.ivan.tutorials.controller;
 import com.polovyi.ivan.tutorials.dto.CreateReportRequest;
 import com.polovyi.ivan.tutorials.dto.CustomerResponse;
 import com.polovyi.ivan.tutorials.service.CustomerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
-@RequestMapping(path = "/v1/customers")
 @RestController
-public record CustomerController(CustomerService customerService) {
+@RequiredArgsConstructor
+@RequestMapping(path = "/v1/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
